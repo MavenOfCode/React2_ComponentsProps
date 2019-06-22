1884c2b5198847eb1781196cea3cde15da69157b
Suzanne Richman <srichm@ext.uber.com>
2019-06-22 13:36:29 -0700

    working through udemy course on to component reuse and advance props implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div>
+      <h1>UDemyCourse React</h1>
+      <h2>Container practice: June 2019</h2>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <div className="ui container comments">
+      <CommentDetail
+      image={faker.image.avatar()}
+      author="Sam" timeAgo="Today at 4:55pm"
+      content="Great idea!"
+      />
+      <CommentDetail
+      image={faker.image.avatar()}
+      author="Max"
+      timeAgo="Today at 2:05pm"
+      content="I'm in!"
+      />
+      <CommentDetail
+      image={faker.image.avatar()}
+      author="Allo"
+      timeAgo="Yesterday at 10:55am"
+      content="Let's do it!!"
+      />
+      <CommentDetail
+      image={faker.image.avatar()}
+      author="Tay"
+      timeAgo="Yesterday at 7:03am"
+      content="Anyone want to go to Golden Gardens at 6pm tomorrow for a BBQ potlock?"
+      />
+      </div>
+        <br/>
+        <p style={{fontStyle:'italic', fontSize:'1em'}}>     Created using faker, react, reactDOM, JSX</p>
+      </div>
+  )
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
